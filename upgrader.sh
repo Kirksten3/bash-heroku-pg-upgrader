167fb7be69975fe208b37a3e730534a77be0a2a1
Kirk Stennett <kirkstennett@01199-MBP15.local>
2016-06-22 12:16:09 -0700

    updates to bash version, adding a test delete function to test the database additions

--- upgrader.sh
+++ upgrader.sh
@@ -7,9 +7,15 @@ clear
 state="start"
 name=""
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+
 function menu {
     printf "Heroku PG Upgrader Main Menu\n"
-    printf "(1) Set / Change Heroku appname \"$name\"\n"
+    printf "(1) Set / Change Heroku appname \"${BLUE}$name${NC}\"\n"
     if [ $state == "joined" ]
         then
             printf "(2) DB Upgrade Menu\n"
@@ -30,19 +36,19 @@ function menu {
             fi
             ;;
         
-        "x")
+        [xX])
             exit 0
             ;;
     esac
 }
 
 function database_upgrader_menu {
-    printf "Database Upgrade Menu for \"$name\"\n"
-    printf "(1) View pg:info + pg:backups schedules + dynos\n"
+    printf "Database Upgrade Menu for \"${BLUE}$name${NC}\"\n"
+    printf "(1) View ${GREEN}pg:info${NC} + ${GREEN}pg:backups schedules${NC} + ${GREEN}dynos${NC}\n"
     if [ "$state" != "addon_created" ]
         then
             # option 2 will go here
-            printf "(2) Create new addon\n"
+            printf "(2) Create new pg addon\n"
     fi
     if [ "$state" == "addon_created" ]
         then
@@ -52,6 +58,7 @@ function database_upgrader_menu {
     printf "(x) Exit to Main Menu\n"
     printf "Choice: "
     read choice
+    
     case "$choice" in
         "1")
             printf "\npg:info\n-------\n"
@@ -75,8 +82,11 @@ function database_upgrader_menu {
             ;;
         "10")
             ;;
-        "x")
+        [xX])
             menu
+            ;;
+        [dD])
+            test_delete_mode
             ;;
     esac
 }
@@ -101,6 +111,37 @@ function join_app {
     # visual spacing
     printf "\n"
     database_upgrader_menu 
+}
+
+function create_addon {
+    printf "Create a (b)asic, (F)ree, or (S)tandard PG DB?\n"
+    printf "Choice: "
+    read db_type
+    case "$db_type" in
+        [bB])
+            add_on=`heroku addons:create heroku-postgresql:hobby-basic -a $name`
+            ;;
+        [fF])
+            add_on=`heroku addons:create heroku-postgresql:hobby-dev -a $name`
+            ;;
+        [sS])
+            add_on=`heroku addons:create heroku-postgresql:standard -a $name`
+            ;;
+    esac
+}
+
+function test_delete_mode {
+    printf "${RED}ENTERED TEST DELETE MODE${NC}\n"
+    echo "${pg_info}"
+    printf "\nWhich database should be deleted? Enter exact name.\n"
+    printf "Choice: "
+    read db_delete
+    printf "\nType CONFIRM to confirm the deletion: "
+    read confirm
+    case "$confirm" in
+        [cC][oO][nN][fF][iI][rR][mM])
+            
+            ;;
 }
 
 menu
